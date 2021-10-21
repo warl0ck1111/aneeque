6065377b61b01c338454b3d12bc4ee70705259f7
Okala III <bashirokala@gmail.com>
2021-10-21 16:26:47 +0100

    initial commit; initialized project with dependencies and database configurations

---
+++ src/test/java/com/example/usermgnt/UserMgntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usermgnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMgntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
